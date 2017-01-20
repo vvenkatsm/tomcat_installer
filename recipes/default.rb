d30b8649ed9ca1a75ae8dae31c53f8ef25e8f1c4
vishraparthi9 <vish.raparthi@gmail.com>
2017-01-20 11:18:50 -0800

    Comments for detailed explanation

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,12 +6,17 @@
 
 case node['platform']
 when 'debian', 'ubuntu'
-  include_recipe 'apt'
-  package 'curl' 
+  include_recipe 'apt'      
+  package 'curl'             # Why do you need curl package if it is debian/Ubuntu? Where are you using it?
 end
 
 include_recipe 'java'
 
+
+# What is "application" resource? Where does it come from? 
+# I don't see an LWRP (Providers/Resources) or class definition (in Libraries) 
+# How does this install tomcat?
+# please explain in detail
 application node[:tomcat_installer][:application_name] do 
   path node[:tomcat_installer][:application_path]
   repository node[:tomcat_installer][:war_uri]
@@ -23,6 +28,10 @@ application node[:tomcat_installer][:application_name] do
   notifies :run, 'execute[restart_tomcat]', :delayed
 end
 
+
+# Also, can you explain how the notifies action in this resource and the above resource work?
+# Will it first install tomcat, deploy war, restart tomcat and then send email or
+# install tomcat, deploy war, send email and then restart?
 execute 'restart_tomcat' do
   supports status: true, restart: true, reload: true
   action [:enable, :start]
